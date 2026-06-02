90433a1bfc3554540db1b50ef7fe5e3899b1edb7
Pedro Henrique Alves Maranhão <pedrinhoelloco@outlook.com.br>
2026-06-02 16:25:02 -0300

    feat: atenticar

--- src/app/pages/autenticar/autenticar.ts
+++ src/app/pages/autenticar/autenticar.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-autenticar',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './autenticar.html',
   styleUrl: './autenticar.css',
 })
-export class Autenticar {}
+export class Autenticar {
+
+  http = inject(HttpClient);
+
+  formAutenticar = new FormGroup({
+    email : new FormControl('', [Validators.required, Validators.email]),
+    senha : new FormControl('', [Validators.required])
+  });
+
+  autenticar() {
+
+    this.http.post('http://localhost:8082/api/usuario/autenticar', this.formAutenticar.value)
+      .subscribe({ 
+            next: (data) => { 
+              console.log(data); 
+              this.formAutenticar.reset();  
+            }, 
+            error: (e) => { 
+              console.log(e.error); 
+            } 
+      });
+  }
+
+}
